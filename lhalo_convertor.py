a06b9bece5c4517ce876a8a2a70eb92e087367d5
Adam Ussing <adam.ussing@gmail.com>
2021-08-17 17:00:21 +1000

    Added read lines for header attributes, started database read

--- lhalo_convertor.py
+++ lhalo_convertor.py
@@ -5,7 +5,19 @@ trees = '/Users/101125182/Documents/2021/data/DM_L50_N128_output/trees.hdf5'
 
 tree_data = h5py.File(trees, 'r')
 
-config = tree_data.require_group('Config').attrs
+# config = tree_data.require_group('Config').attrs
 header = tree_data.require_group('Header').attrs
 
+N_Trees = header.__getitem__('Ntrees_Total')
+tot_NHalos = header.__getitem__('Nhalos_Total')
+# Tree_NHalos = header.__getitem__('NHalos_per_tree') ## Not sure where value recorded, if at all
+# ParticleMass = header.__getitem__('Pat_Mass') ## Not in Header
+NumSimTreeFiles = header.__getitem__('NumFiles')
+
+tree_halos = tree_data.require_group('TreeHalos')
+# tree_tables = tree_data.require_group('TreeTable')
+# tree_times = tree_data.require_group('TreeTimes')
+
+snap_number = tree_halos.__getitem__('SnapNum')  ## snap_number set as array
+# snap_number = list(tree_data['TreeHalos/SnapNum'])  ## list of all snap numbers for halos
 
