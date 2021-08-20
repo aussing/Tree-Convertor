298802e6737e8c6774b33428f33ca73cff7397f8
Adam Ussing <adam.ussing@gmail.com>
2021-08-20 15:15:32 +1000

    lhalo params mostly accounted for

--- lhalo_convertor.py
+++ lhalo_convertor.py
@@ -5,19 +5,36 @@ trees = '/Users/101125182/Documents/2021/data/DM_L50_N128_output/trees.hdf5'
 
 tree_data = h5py.File(trees, 'r')
 
-# config = tree_data.require_group('Config').attrs
+## Header Properties
 header = tree_data.require_group('Header').attrs
-
-N_Trees = header.__getitem__('Ntrees_Total')
-tot_NHalos = header.__getitem__('Nhalos_Total')
-# Tree_NHalos = header.__getitem__('NHalos_per_tree') ## Not sure where value recorded, if at all
-# ParticleMass = header.__getitem__('Pat_Mass') ## Not in Header
+N_Trees = header.__getitem__('Ntrees_Total')  #first thing to be read in
+tot_NHalos = header.__getitem__('Nhalos_Total') # Second thing read in
+# Tree_NHalos = header.__getitem__('NHalos_per_tree') 
+# ParticleMass = header.__getitem__('Pat_Mass') 
 NumSimTreeFiles = header.__getitem__('NumFiles')
 
-tree_halos = tree_data.require_group('TreeHalos')
-# tree_tables = tree_data.require_group('TreeTable')
-# tree_times = tree_data.require_group('TreeTimes')
+## 
+SnapNum = tree_data['TreeHalos/SnapNum']
+FileNr = header.__getitem__('NumFiles') ## Only have outputs w/ 1 tree file atm, not sure where parameter for file number will be stored if at all
+SubhaloIndex = tree_data['TreeHalos/TreeID']
+SubHalfMass = tree_data['####']  
 
-snap_number = tree_halos.__getitem__('SnapNum')  ## snap_number set as array
-# snap_number = list(tree_data['TreeHalos/SnapNum'])  ## list of all snap numbers for halos
+## Merger tree pointers
+Descendant = tree_data['TreeHalos/TreeDescendant']
+FirstProgenitor = tree_data['TreeHalos/TreeFirstProgenitor']
+NextProgenitor = tree_data['TreeHalos/TreeNextProgenitor']
+FirstHaloInFOFgroup = tree_data['TreeHalos/FirstHaloInFOFGroup']
+NextHaloInFOFgroup = tree_data['TreeHalos/NextHaloInFOFGroup']
+
+
+##Sub halo Properties
+
+Len = tree_data['TreeHalos/Len']
+Mvir = tree_data['TreeHalos/Group_M_Crit200']
+Pos = tree_data['TreeHalos/SubhaloPos']
+Vel = tree_data['TreeHalos/SubhaloVel']
+VelDisp = tree_data['TreeHalos/SubhaloVelDisp']
+Vmax = tree_data['TreeHalos/SubhaloVmax']
+Spin = tree_data['TreeHalos/SubhaloSpin']
+MostBoundID = tree_data['TreeHalos/SubhaloIDMostBound']
 
